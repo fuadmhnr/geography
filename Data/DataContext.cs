4086f7dce7c92d0dd770c2a0ca51f19ca068bb86
fuadmhnr <fuadmuhammadn@gmail.com>
2023-05-09 11:01:01 +0700

    feat: create data context, connection to db and migrations

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Geography.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Geography.Data;
+
+public class DataContext : DbContext
+{
+  public DataContext(DbContextOptions<DataContext> options) : base(options)
+  {
+  }
+
+  public DbSet<Country> Countries { get; set; }
+}
---
+++ Migrations/20230509035643_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace geography.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
